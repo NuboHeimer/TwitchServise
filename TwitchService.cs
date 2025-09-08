a7a0870f0bc9e2e8f485d67333017e7a33c6254a
NuboHeimer <NuboHeimer@yandex.ru>
2025-09-08 18:49:53 +0300

    Добавил gitignore. Добавил метод для получения событи прихода и ухода зрителя. Изменил метод AddFirestWordViewer, теперь добавляет и в twitchPreviousPresentViewers. Обновил уровни логирования, добавил проброс ошибки. Вынес формирование события для миничата в отдельный метод.

--- TwitchService.cs
+++ TwitchService.cs
@@ -9,35 +9,36 @@
 using System;
 using System.Collections.Generic;
 using System.Threading;
+using System; //дублирование нужно, что бы при find refs стримербот добавил System.Core.dll, необходимый для HashSet. Иначе его надо добавлять руками. Я не знаю, почему это так работает.
 
 public class CPHInline
 {
     public void Init()
     {
         CPH.LogInfo("[TwitchService] initialized.");
-        if (CPH.GetGlobalVar<List<string>>("twitchTodaysViewers", true) == null)
+        if (CPH.GetGlobalVar<HashSet<string>>("twitchTodaysViewers", true) == null)
         {
-            CPH.SetGlobalVar("twitchTodaysViewers", new List<string>(), true);
-            CPH.LogInfo("[TwitchService] Global variable twitchTodaysViewers created.");
+            CPH.SetGlobalVar("twitchTodaysViewers", new HashSet<string>(), true);
+            CPH.LogDebug("[TwitchService] Global variable twitchTodaysViewers created.");
         }
 
-        if (CPH.GetGlobalVar<List<string>>("twitchPreviousPresentViewers", true) == null)
+        if (CPH.GetGlobalVar<HashSet<string>>("twitchPreviousPresentViewers", true) == null)
         {
-            CPH.SetGlobalVar("twitchPreviousPresentViewers", new List<string>(), true);
-            CPH.LogInfo("[TwitchService] Global variable twitchPreviousPresentViewers created.");
+            CPH.SetGlobalVar("twitchPreviousPresentViewers", new HashSet<string>(), true);
+            CPH.LogDebug("[TwitchService] Global variable twitchPreviousPresentViewers created.");
         }
     }
 
     public bool GetNewViewers()
     {
-        List<string> twitchTodaysViewers = CPH.GetGlobalVar<List<string>>("twitchTodaysViewers", true);
+        HashSet<string> twitchTodaysViewers = CPH.GetGlobalVar<HashSet<string>>("twitchTodaysViewers", true);
         try
         {
-            CPH.LogInfo("[TwitchService] try to get new viewers");
-            var currentViewers = (List<Dictionary<string, object>>)args["users"];
+            CPH.LogDebug("[TwitchService][GetNewViewers] try to get new viewers");
+            List<Dictionary<string, object>> currentViewers = (List<Dictionary<string, object>>)args["users"];
             if (currentViewers.Count == 0)
             {
-                CPH.LogInfo("[TwitchService] Viewers not found.");
+                CPH.LogDebug("[TwitchService][GetNewViewers] Viewers not found.");
                 return false;
             }
 
@@ -45,19 +46,62 @@ public class CPHInline
             {
                 if (!twitchTodaysViewers.Contains(viewer["userName"].ToString()))
                 {
+                    CreateViewerEvent(viewer["userName"].ToString(), "Зашел сегодня на трансляцию первый раз.");
                     twitchTodaysViewers.Add(viewer["userName"].ToString());
-                    CPH.SetArgument("service", "Twitch");
-                    CPH.SetArgument("title", "Новый зритель");
-                    CPH.SetArgument("message", viewer["userName"].ToString());
-                    CPH.ExecuteMethod("MiniChat Method Collection", "CreateCustomEvent");
-                    Thread.Sleep(200); // если убрать задержку, то при большом количестве одновременно зашедших зрителей некоторые оповещения могут не отобразиться.
                 }
             }
             CPH.SetGlobalVar("twitchTodaysViewers", twitchTodaysViewers, true);
         }
         catch (Exception e)
         {
-            CPH.LogError("[TwitchService] Some error was happend.");
+            CPH.LogError("[TwitchService][GetNewViewers] Some error was happened." + e.Message);
+        }
+
+        return true;
+    }
+
+    public bool GetInOutViewers()
+    {
+        HashSet<string> twitchPreviousPresentViewers = CPH.GetGlobalVar<HashSet<string>>("twitchPreviousPresentViewers", true);
+        try
+        {
+            CPH.LogDebug("[TwitchService][GetInOutViewers] try to get viewers");
+            List<Dictionary<string, object>> currentViewers = (List<Dictionary<string, object>>)args["users"];
+
+            if (currentViewers.Count == 0)
+            {
+                CPH.LogDebug("[TwitchService][GetInOutViewers] Viewers not found.");
+                return false;
+            }
+
+            HashSet<string> currentViewersNames = new HashSet<string>();
+
+            foreach (var viewer in currentViewers)
+            {
+                currentViewersNames.Add(viewer["userName"].ToString());
+            }
+
+            foreach (var viewerName in currentViewersNames)
+            {
+                if (!twitchPreviousPresentViewers.Contains(viewerName))
+                {
+                    CreateViewerEvent(viewerName, "Обнаружен(а) в списке зрителей.");
+                }
+            }
+
+            foreach (var previousViewer in twitchPreviousPresentViewers)
+            {
+                if (!currentViewersNames.Contains(previousViewer))
+                {
+                    CreateViewerEvent(previousViewer, "Пропал(а) из списка зрителей.");
+                }
+            }
+
+            CPH.SetGlobalVar("twitchPreviousPresentViewers", currentViewersNames, true);
+        }
+        catch (Exception e)
+        {
+            CPH.LogError("[TwitchService][GetInOutViewers] Some error was happened." + e.Message);
         }
 
         return true;
@@ -65,26 +109,26 @@ public class CPHInline
 
     public bool AddFirstWordViewer()
     {
-        List<string> twitchTodaysViewers = CPH.GetGlobalVar<List<string>>("twitchTodaysViewers", true);
-        twitchTodaysViewers.Add(args["userName"].ToString());
+        HashSet<string> twitchTodaysViewers = CPH.GetGlobalVar<HashSet<string>>("twitchTodaysViewers", true);
+        HashSet<string> twitchPreviousPresentViewers = CPH.GetGlobalVar<HashSet<string>>("twitchPreviousPresentViewers", true);
+        string userName = args["userName"].ToString();
+
+        twitchTodaysViewers.Add(userName);
+        twitchPreviousPresentViewers.Add(userName);
+
         CPH.SetGlobalVar("twitchTodaysViewers", twitchTodaysViewers, true);
+        CPH.SetGlobalVar("twitchPreviousPresentViewers", twitchPreviousPresentViewers, true);
+
         return true;
     }
     public bool ClearTodaysViewers()
     {
-        CPH.SetGlobalVar("twitchTodaysViewers", new List<string>(), true);
+        CPH.SetGlobalVar("twitchTodaysViewers", new HashSet<string>(), true);
         return true;
     }
-
-    public bool ClearTodaysViewers()
-    {
-        CPH.SetGlobalVar("twitchTodaysViewers", new List<string>(), true);
-        return true;
-    }
-
     public bool ClearPreviousPresentViewers()
     {
-        CPH.SetGlobalVar("twitchPreviousPresentViewers", new List<string>(), true);
+        CPH.SetGlobalVar("twitchPreviousPresentViewers", new HashSet<string>(), true);
         return true;
     }
 
@@ -98,5 +142,14 @@ public class CPHInline
     {
         CPH.UnsetGlobalVar("twitchLastViewersNameList", true);
         return true;
+    }
+
+    private void CreateViewerEvent(string viewerName, string eventType)
+    {
+        CPH.SetArgument("service", "Twitch");
+        CPH.SetArgument("title", viewerName);
+        CPH.SetArgument("message", eventType);
+        CPH.ExecuteMethod("MiniChat Method Collection", "CreateCustomEvent");
+        Thread.Sleep(200); // если убрать задержку, то при большом количестве одновременно зашедших зрителей некоторые оповещения могут не отобразиться.
     }
 }
